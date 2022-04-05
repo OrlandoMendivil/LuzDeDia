0aa4d8b289dd841d0601b78b8baa9a9d867295b1
OrlandoMendivil <omendivil6@gmail.com>
2022-04-04 23:51:04 -0700

    Pantalla de inicio de sesión

---
+++ app/src/androidTest/java/mendivil/valenzuela/ciulog/luzdedia/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package mendivil.valenzuela.ciulog.luzdedia
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("mendivil.valenzuela.ciulog.luzdedia", appContext.packageName)
+    }
+}
